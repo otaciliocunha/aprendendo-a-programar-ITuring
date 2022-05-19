fc24f5e9bbba02f5cedf081f293c7c3416caced4
Your Name <you@example.com>
2022-05-19 14:10:19 -0300

    Criação do codigo

---
+++ dia-3/teste.js
@@ -0,0 +1,83 @@
+console.log("Olá mundão!")
+console.log("Meu primeiro programa da ITuring!")
+
+// Isso é um comentário de uma linha
+
+/* Isso é um comentário de
+várias linhas
+acabando aqui*/
+
+//declaração de Variaveis
+let coisas
+let coisa1, coisa2
+
+//Atribuição de valor a variável
+coisas = 'quadado'
+console.log(coisas,coisa1,coisa2)
+coisa1 = 12
+console.log(coisas,coisa1,coisa2)
+coisa1 = true
+
+console.log(coisas,coisa1,coisa2)
+
+//let nome
+//nome = Instrutora Jô"
+let nome = "Instrutora Jô"
+let sobrenome = "Corrêa"
+let idade = 38
+let idadetexto = "38"
+let autura = 1.61
+let logada = true
+let logadatexto ='true'
+
+//console.log(nome, typeof(nome))
+//console.log(sobrenome, typeof(sobrenome))
+//console.log(idade, typeof(idade))
+//console.log(idadetexto, typeof(idadetexto))
+//console.log(autura, typeof(autura))
+//console.log(logada, typeof(logada))
+//console.log(logadatexto, typeof(idadetexto))
+
+//Array
+//let lista = ['arroz', 'feijão', 'batata', nome, 12, true, [1,2,3]]
+//console.log(lista, typeof(lista))
+//console.log(lista[0])
+//console.log(lista[1])
+//Objeto
+//let mapa = {nome : 'Jo', idade: 38}
+//console.log(mapa, typeof(mapa))
+//console.log(mapa['nome'])
+
+let numero = 13
+console.log(numero, typeof(numero))
+numero = 'treze'
+console.log(numero, typeof(numero))
+
+let nota1 = 8
+let nota2 = 10
+
+let media = (nota1 + nota2) /2
+console.log('media:', media)
+
+let resultado = 10 - 3
+console.log('resultado:', resultado)
+
+let num1 = 1
+let num2 = '2'
+let resultadoMaluco = num1 + num2
+console.log('resultadoMaluco', resultadoMaluco)
+
+let resultadoCorreto = num1 - num2
+console.log('resultadoCorreto', resultadoCorreto)
+
+console.log(nome + " " +sobrenome)
+
+//condicionais
+let idadeJogador = 18
+
+if(idadeJogador == 18){
+console.log('Pode jogar')
+}else{
+//se não for verdade
+console.log('Não pode jogar')
+}
